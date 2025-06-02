548bb2e20822a91cb7886c3f528917812cc26a9f
VibeSyntax <138536097+beza102@users.noreply.github.com>
2025-06-02 15:22:59 -0700

    methods for reachableArea possibleMoves

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -32,6 +32,51 @@ public class ExplorerSearch {
         // Implement your method here!
         // Please also make more test cases
         // I STRONGLY RECOMMEND testing some helpers you might make too
-        return -1;
+        int[] start = findStart(island);
+        boolean[][] visited = new boolean[island.length][island[0].length];
+        return reachableArea(island, start, visited);
+    }
+
+    //Helper method to explore reachabe area
+    public static int reachableArea(int[][]island, int[] current, boolean[][] visited){
+        int r = current[0];
+        int c = current[1];
+        
+        //if visited stop exploring
+        if(visited[r][c]) return 0;
+        visited[r][c] = true;
+        int count = 1;
+
+        List<int[]> neighbors = possibleMoves(island, current);
+        for(int[] neighbor: neighbors){
+            count += reachableArea(island, neighbor, visited);
+        }
+        return count;  
+    }
+
+    //find the start location 0
+     public static int[] findStart(int[][] island) {
+        for (int r = 0; r < island.length; r++) {
+            for (int c = 0; c < island[r].length; c++) {
+                if (island[r][c] == 0) {
+                    return new int[]{r, c};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No starting point (0) found on the island");
+    }
+
+    //find possible moves
+    public static List<int[]> possibleMoves(int[][] island, int[] current) {
+        List<int[]> moves = new ArrayList<>();
+        int curR = current[0];
+        int curC = current[1];
+
+        // Up
+        int newR = curR - 1;
+        int newC = curC;
+        if (newR >= 0 && (island[newR][newC] == 0 || island[newR][newC] == 1)) {
+            moves.add(new int[]{newR, newC});
+        }
     }
 }
